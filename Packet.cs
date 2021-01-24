2f16f9465af5605ed6e65b3e276e0ec7db2ae071
Krystof Zahumensky <zahumensky.krystof@gmail.com>
2021-01-24 23:11:00 +0100

    uwu

---
+++ Packet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UdpNatStun
+{
+    class Packet
+    {
+        public int Cmd { get; set; }
+        public string Message { get; set; }
+
+        public override string ToString()
+        {
+            return $"[{Cmd}][{Message}]";
+        }
+        public Packet(int cmd, string msg)
+        {
+            Cmd = cmd;
+            Message = msg;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using STUN;
 using STUN.Attributes;
@@ -11,6 +12,9 @@ namespace UdpNatStun
 {
     class Program
     {
+        public static Regex Re = new Regex(@"\[(?<cmd>[0-9])\]\[(?<value>.*)\]");
+        public static List<IPEndPoint> remoteEps = new List<IPEndPoint>();
+        private static Thread keepAlive = new Thread(KeepAliveSend);
         static void Main(string[] args)
         {
             // STUNUtils.TryParseHostAndPort("stun.l.google.com", out IPEndPoint stunEndPoint);
@@ -37,8 +41,9 @@ namespace UdpNatStun
 
             var th = new Thread(Receive);
             th.Start(udpClient);
+            keepAlive.Start(udpClient);
 
-            var remoteEps = new List<IPEndPoint>();
+            
 
             while (true)
             {
@@ -55,6 +60,11 @@ namespace UdpNatStun
 
                     var ms = Encoding.ASCII.GetBytes("hewwo");
                     udpClient.Send(ms, ms.Length, remoteEp);
+                    var ms2 = Encoding.ASCII.GetBytes($"[2][{magicToken}]");
+                    foreach (var ep in remoteEps)
+                    {
+                        udpClient.Send(ms2, ms2.Length, ep);
+                    }
                 }
                 else if (cmd.StartsWith("/l"))
                 {
@@ -63,9 +73,17 @@ namespace UdpNatStun
                 }
                 else
                 {
-                    var ms = Encoding.ASCII.GetBytes(cmd);
+                    var ms = Encoding.ASCII.GetBytes($"[0][{cmd}]");
                     foreach (var ep in remoteEps)
-                        udpClient.Send(ms, ms.Length, ep);
+                        try
+                        {
+                            udpClient.Send(ms, ms.Length, ep);
+                        }
+                        catch (Exception e)
+                        {
+
+                            Console.WriteLine(e.Message);
+                        }
                 }
             }
         }
@@ -82,6 +100,44 @@ namespace UdpNatStun
                     var remoteEp = new IPEndPoint(IPAddress.Any, 0);
                     client.AllowNatTraversal(true);
                     var ms = client.Receive(ref remoteEp);
+                    var ss = Encoding.ASCII.GetString(ms);
+
+                    var result = Re.Match(ss);
+                    if (result.Success)
+                    {
+                        var packet = new Packet(int.Parse(result.Groups["cmd"].Value), result.Groups["value"].Value);
+                        switch (packet.Cmd)
+                        {
+                            case 0:
+                                Console.WriteLine(packet.Message);
+                                break;
+                            case 1:
+                                var msg = Encoding.ASCII.GetBytes("[2][]");
+                                client.Send(msg,msg.Length, remoteEp);
+                                break;
+                            case 3:
+                                var magicPackets = packet.Message.Split(';');
+                                var adds = new List<string>();
+                                foreach(var pckt in magicPackets)
+                                {
+                                    var ep = ToEndpoint(pckt);
+                                    if (remoteEps.Contains(ep))
+                                    {
+                                        continue;
+                                    }
+                                    remoteEps.Add(ep);
+                                    adds.Add(pckt);
+                                }
+
+                                var res = Encoding.ASCII.GetBytes(string.Join(';', adds));
+                                foreach (var ep in remoteEps)
+                                {
+                                    client.Send(res, res.Length, ep);
+                                }
+                                break;
+                        }
+                        
+                    }
                     Console.WriteLine($"{remoteEp} -> {Encoding.ASCII.GetString(ms)}");
                 }
                 catch(Exception e)
@@ -89,6 +145,43 @@ namespace UdpNatStun
                     Console.WriteLine(e.Message);
                 }
                 
+            }
+        }
+        public static IPEndPoint ToEndpoint(string token)
+        {
+            try
+            {
+                var magicToken = Convert.FromBase64String(token);
+                var remoteEp = new IPEndPoint(
+                    new IPAddress(magicToken[0..^2]),
+                    BitConverter.ToUInt16(magicToken[^2..]));
+
+                return remoteEp;
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+        public static void KeepAliveSend(object args)
+        {
+            var client = args as UdpClient;
+            while (true)
+            {
+                Thread.Sleep(30000);
+                var bytes = Encoding.ASCII.GetBytes("[1][]");
+                foreach (var ep in remoteEps)
+                {
+                    try
+                    {
+                        client.Send(bytes, bytes.Length, ep);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                   
+                }
             }
         }
     }
